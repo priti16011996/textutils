1744e9dc77043004b5bf4ada3ebcb584da4ed5ff
Priti <priti16maurya@gmail.com>
2025-12-07 13:20:36 +0530

    all related changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbars from './components/Navbars';
-//import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
+import Alerts from './components/Alerts';
+import React,{ useState } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light'); //Whether dark mode is enabled or not
+  const [alert,setAlert]=useState(null);
+  const showAlertPopupMessageBasedOnServirityLevel=(message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500)
+  }
+  const toggle=()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+      showAlertPopupMessageBasedOnServirityLevel("Dark mode has been enabled","Success");
+      document.title='TextUtils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlertPopupMessageBasedOnServirityLevel("Light mode has been enabled","Success");
+      document.title='TextUtils - Light Mode';
+    }
+
+  }
   return (
    <>
-    <Navbars title="TextUtils" listItem1="Home" listItem2="About"/>
-    <div className="container" >
-      {/* <TextForm title="Enter Text To Analyze Below"/> */}
-      <About />
-    </div>
+   {/* <Router> */}
+      <Navbars title="TextUtils" listItem1="Home" listItem2="About"  mode={mode} toggleMode={toggle}/>
+      <Alerts alert={alert}/>
+      <div className="container" >
+        {/* <Routes>
+            <Route exact
+              path="/"
+              element ={
+                <TextForm showAlertPopupMessageBasedOnServirityLevel={showAlertPopupMessageBasedOnServirityLevel} title="Enter Text To Analyze Below" mode={mode}/>
+                } />
+            <Route exact
+              path="/about"
+              element={
+                  <About /> 
+              } />
+        </Routes> */}
+        <TextForm showAlertPopupMessageBasedOnServirityLevel={showAlertPopupMessageBasedOnServirityLevel} title="Enter Text To Analyze Below" mode={mode}/>
+      </div>
+    {/* </Router> */}
    </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,20 +5,35 @@ export default function TextForm(props) {
         //console.log("UpperCase was clicked");
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlertPopupMessageBasedOnServirityLevel("Converted to Uppercase!","Success");
     }
 
     const handleOnChange = (event)=>{
         //console.log("OnChange was clicked")
         setText(event.target.value);
+        props.showAlertPopupMessageBasedOnServirityLevel("Text Changed!","Success");
     }
 
     const handleLpClick =()=>{
         let lowerText = text.toLowerCase();
         setText(lowerText);
+        props.showAlertPopupMessageBasedOnServirityLevel("Converted to Lowercase!","Success");  
     }
     const handleClearClick =()=>{
         let ClearText = '';
         setText(ClearText);
+        props.showAlertPopupMessageBasedOnServirityLevel("Text Cleared!","Success");
+    }
+    const handlerCopyText=()=>{
+        let text = document.getElementById("Box");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlertPopupMessageBasedOnServirityLevel("Text Copied to Clipboard!","Success");    
+    }
+    const handlerRemoveExtraSpaces =()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlertPopupMessageBasedOnServirityLevel("Extra Spaces Removed!","Success");    
     }
     const handleDownloadClick = () => {
         const element = document.createElement("a");
@@ -28,21 +43,24 @@ export default function TextForm(props) {
         document.body.appendChild(element);
         element.click();
         document.body.removeChild(element);
+        props.showAlertPopupMessageBasedOnServirityLevel("Text File Downloaded!","Success");
     };
     const [text, setText] = useState("Enter Your Text Here")
     return (  
         <>
-        <div className="container my-3"> 
+        <div className="container my-3" style={{color:props.mode ==='dark'?'#ffffffff':'#050505ff'}}> 
             <h1>{props.title}</h1>
             <div className="mb-3">
-            <textarea className="form-control" id="Box" value={text} onChange={handleOnChange} rows="8"></textarea>
+            <textarea className="form-control" id="Box" value={text} onChange={handleOnChange} style={{color:props.mode ==='dark'?'#000000ff':'#f8f3f3ff', backgroundColor:props.mode==='dark'?'white':'#908e8eff'}}rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To Upper Case</button>
             <button className="btn btn-primary mx-1" onClick={handleLpClick}>Convert To Lower Case</button>
             <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
             <button className="btn btn-primary mx-1" onClick={handleDownloadClick}>Download Text</button>
+            <button className="btn btn-primary mx-1" onClick={handlerCopyText}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={handlerRemoveExtraSpaces}>Remove Extra Space</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.mode ==='dark'?'#ffffffff':'#050505ff'}}>
             <h2>Your Text Summary</h2>
             <p><b>Total Number of Words in my Text:</b> {text.split(" ").length} </p>
             <p><b>Total Number Of Character in My Text: {text.length}</b></p>
